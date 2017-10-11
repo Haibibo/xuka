9a941f23403adcacdcb368e1d300003842ad30cf
Haibibo <32150097+Haibibo@users.noreply.github.com>
2017-10-11 10:12:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/xuka/xuka/xuka.java
@@ -0,0 +1,13 @@
+package com.example.xuka.xuka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class xuka extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xuka);
+    }
+}
